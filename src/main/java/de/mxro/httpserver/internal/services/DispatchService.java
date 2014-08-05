b3e8d093f9875b07ae121c8379ac3aaea2f0efb2
mxro <mxro@nowhere.com>
2014-08-05 10:18:21 +1000

    Building test cases for new server organization and fixing bugs Autocommit Tue Aug 05 10:18:21 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/DispatchService.java
+++ src/main/java/de/mxro/httpserver/internal/services/DispatchService.java
@@ -17,58 +17,59 @@ import de.mxro.service.callbacks.StartCallback;
 public class DispatchService implements HttpService {
 
 	private final Map<String, HttpService> serviceMap;
-	
+
 	@Override
 	public void process(Request request, Response response,
 			Closure<SuccessFail> callback) {
-		
+
 		final String uri = request.getRequestUri();
 		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
-			if ( uri.startsWith(e.getKey())) {
+			if (uri.startsWith(e.getKey())) {
 				e.getValue().process(request, response, callback);
 				return;
 			}
 		}
-		
+
 		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
 			if (e.getKey().equals("*")) {
 				e.getValue().process(request, response, callback);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void stop(ShutdownCallback callback) {
 		ArrayList<HttpService> services = new ArrayList<HttpService>();
-		
+
 		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
 			services.add(e.getValue());
 		}
-		
+
 		stop(services, 0, callback);
 	}
 
-	private static void stop(final List<HttpService> services, final int serviceIdx, final ShutdownCallback callback) {
+	private static void stop(final List<HttpService> services,
+			final int serviceIdx, final ShutdownCallback callback) {
 		if (serviceIdx >= services.size()) {
 			callback.onShutdownComplete();
 			return;
 		}
-		
+
 		services.get(serviceIdx).stop(new ShutdownCallback() {
-			
+
 			@Override
 			public void onShutdownComplete() {
-				stop(services, serviceIdx+1, callback);
+				stop(services, serviceIdx + 1, callback);
 			}
-			
+
 			@Override
 			public void onFailure(Throwable t) {
 				callback.onFailure(t);
 			}
 		});
 	}
-	
+
 	public DispatchService(Map<String, HttpService> serviceMap) {
 		super();
 		this.serviceMap = serviceMap;
@@ -76,9 +77,21 @@ public class DispatchService implements HttpService {
 
 	@Override
 	public void start(StartCallback callback) {
-		
+		ArrayList<HttpService> services = new ArrayList<HttpService>();
+
+		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
+			services.add(e.getValue());
+		}
+
+		start(services, 0, callback);
 	}
 
-	
-	
+	private static void start(final List<HttpService> services,
+			final int serviceIdx, final StartCallback callback) {
+		if (serviceIdx >= services.size()) {
+			callback.onStarted();
+			return;
+		}
+	}
+
 }
