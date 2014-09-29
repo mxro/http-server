7d907347fc672b5efb1696a34062fd7798eef58b
mxro <mxro@nowhere.com>
2014-09-29 17:23:13 +1000

    Allowing to embed AJ script in HTML pages Autocommit Mon Sep 29 17:23:13 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/FilterService.java
+++ src/main/java/de/mxro/httpserver/internal/services/FilterService.java
@@ -1,7 +1,44 @@
 package de.mxro.httpserver.internal.services;
 
+import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.fn.Closure;
+import de.mxro.fn.SuccessFail;
 import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.Request;
+import de.mxro.httpserver.Response;
 
 public class FilterService implements HttpService {
+
+    private final HttpService primary;
+    private final HttpService secondary;
+
+    @Override
+    public void stop(final SimpleCallback callback) {
+        primary.start(new SimpleCallback() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+
+            @Override
+            public void onSuccess() {
+                secondary.stop(callback);
+            }
+        });
+
+    }
+
+    @Override
+    public void start(final SimpleCallback callback) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void process(final Request request, final Response response, final Closure<SuccessFail> callback) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
