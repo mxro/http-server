3f50dfd094c0d8bb427f0263e8c4f3f373976948
mxro <mxro@nowhere.com>
2014-09-29 17:25:14 +1000

    Allowing to embed AJ script in HTML pages Autocommit Mon Sep 29 17:25:14 EST 2014

--- src/main/java/de/mxro/httpserver/services/Services.java
+++ src/main/java/de/mxro/httpserver/services/Services.java
@@ -2,26 +2,32 @@ package de.mxro.httpserver.services;
 
 import java.util.Map;
 
+import de.mxro.fn.Function;
 import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.Request;
 import de.mxro.httpserver.internal.services.DispatchService;
+import de.mxro.httpserver.internal.services.FilterService;
 import de.mxro.httpserver.internal.services.RequestTimeEnforcerService;
 import de.mxro.httpserver.internal.services.SafeShutdownService;
 
 public class Services {
 
-	public final static HttpService dispatcher(Map<String, HttpService> serviceMap) {
-		return Services.safeShutdown(new DispatchService(serviceMap));
-	}
+    public final static HttpService dispatcher(final Map<String, HttpService> serviceMap) {
+        return Services.safeShutdown(new DispatchService(serviceMap));
+    }
 
-	public static HttpService safeShutdown(HttpService service) {
-		return new SafeShutdownService(service);
-	}
-	
-	
-	public static HttpService limitTime(long maxCallTimeInMs, HttpService decoratedService) {
-		return new RequestTimeEnforcerService(maxCallTimeInMs, decoratedService);
-		
-	}
-	
-	
+    public static HttpService safeShutdown(final HttpService service) {
+        return new SafeShutdownService(service);
+    }
+
+    public static HttpService limitTime(final long maxCallTimeInMs, final HttpService decoratedService) {
+        return new RequestTimeEnforcerService(maxCallTimeInMs, decoratedService);
+
+    }
+
+    public final static HttpService filter(final Function<Request, Boolean> test, final HttpService primary,
+            final HttpService secondary) {
+        return new FilterService(test, primary, secondary);
+    }
+
 }
