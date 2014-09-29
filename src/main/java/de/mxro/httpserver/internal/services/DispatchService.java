60f3f27a025582b1f98687232fbb7871ad721037
mxro <mxro@nowhere.com>
2014-09-29 17:18:13 +1000

    Allowing to embed AJ script in HTML pages Autocommit Mon Sep 29 17:18:13 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/DispatchService.java
+++ src/main/java/de/mxro/httpserver/internal/services/DispatchService.java
@@ -14,96 +14,94 @@ import de.mxro.httpserver.Response;
 
 public class DispatchService implements HttpService {
 
-	private final Map<String, HttpService> serviceMap;
+    private final Map<String, HttpService> serviceMap;
 
-	@Override
-	public void process(Request request, Response response,
-			Closure<SuccessFail> callback) {
+    @Override
+    public void process(final Request request, final Response response, final Closure<SuccessFail> callback) {
 
-		final String uri = request.getRequestUri();
-		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
-			if (uri.startsWith(e.getKey())) {
-				e.getValue().process(request, response, callback);
-				return;
-			}
-		}
+        final String uri = request.getRequestUri();
+        for (final Entry<String, HttpService> e : serviceMap.entrySet()) {
+            if (uri.startsWith(e.getKey())) {
+                e.getValue().process(request, response, callback);
+                return;
+            }
+        }
 
-		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
-			if (e.getKey().equals("*")) {
-				e.getValue().process(request, response, callback);
-			}
-		}
+        for (final Entry<String, HttpService> e : serviceMap.entrySet()) {
+            if (e.getKey().equals("*")) {
+                e.getValue().process(request, response, callback);
+                return;
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public void stop(SimpleCallback callback) {
-		ArrayList<HttpService> services = new ArrayList<HttpService>();
+    @Override
+    public void stop(final SimpleCallback callback) {
+        final ArrayList<HttpService> services = new ArrayList<HttpService>();
 
-		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
-			services.add(e.getValue());
-		}
+        for (final Entry<String, HttpService> e : serviceMap.entrySet()) {
+            services.add(e.getValue());
+        }
 
-		stop(services, 0, callback);
-	}
+        stop(services, 0, callback);
+    }
 
-	private static void stop(final List<HttpService> services,
-			final int serviceIdx, final SimpleCallback callback) {
-		if (serviceIdx >= services.size()) {
-			callback.onSuccess();
-			return;
-		}
+    private static void stop(final List<HttpService> services, final int serviceIdx, final SimpleCallback callback) {
+        if (serviceIdx >= services.size()) {
+            callback.onSuccess();
+            return;
+        }
 
-		services.get(serviceIdx).stop(new SimpleCallback() {
+        services.get(serviceIdx).stop(new SimpleCallback() {
 
-			@Override
-			public void onSuccess() {
-				stop(services, serviceIdx + 1, callback);
-			}
+            @Override
+            public void onSuccess() {
+                stop(services, serviceIdx + 1, callback);
+            }
 
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
-		});
-	}
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+        });
+    }
 
-	public DispatchService(Map<String, HttpService> serviceMap) {
-		super();
-		this.serviceMap = serviceMap;
-	}
+    public DispatchService(final Map<String, HttpService> serviceMap) {
+        super();
+        this.serviceMap = serviceMap;
+    }
 
-	@Override
-	public void start(SimpleCallback callback) {
-		
-		ArrayList<HttpService> services = new ArrayList<HttpService>();
+    @Override
+    public void start(final SimpleCallback callback) {
 
-		for (Entry<String, HttpService> e : serviceMap.entrySet()) {
-			services.add(e.getValue());
-		}
+        final ArrayList<HttpService> services = new ArrayList<HttpService>();
 
-		start(services, 0, callback);
-	}
+        for (final Entry<String, HttpService> e : serviceMap.entrySet()) {
+            services.add(e.getValue());
+        }
 
-	private static void start(final List<HttpService> services,
-			final int serviceIdx, final SimpleCallback callback) {
-		if (serviceIdx >= services.size()) {
-			callback.onSuccess();
-			return;
-		}
-		
-		services.get(serviceIdx).start(new SimpleCallback() {
-			
-			@Override
-			public void onSuccess() {
-				start(services, serviceIdx+1, callback);
-			}
-			
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
-		});
-	}
+        start(services, 0, callback);
+    }
+
+    private static void start(final List<HttpService> services, final int serviceIdx, final SimpleCallback callback) {
+        if (serviceIdx >= services.size()) {
+            callback.onSuccess();
+            return;
+        }
+
+        services.get(serviceIdx).start(new SimpleCallback() {
+
+            @Override
+            public void onSuccess() {
+                start(services, serviceIdx + 1, callback);
+            }
+
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+        });
+    }
 
 }
