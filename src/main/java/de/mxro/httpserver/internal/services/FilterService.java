9f754fd1c44660e072c56b7c0011291c1181c892
mxro <mxro@nowhere.com>
2014-09-29 17:24:13 +1000

    Allowing to embed AJ script in HTML pages Autocommit Mon Sep 29 17:24:13 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/FilterService.java
+++ src/main/java/de/mxro/httpserver/internal/services/FilterService.java
@@ -2,6 +2,7 @@ package de.mxro.httpserver.internal.services;
 
 import de.mxro.async.callbacks.SimpleCallback;
 import de.mxro.fn.Closure;
+import de.mxro.fn.Function;
 import de.mxro.fn.SuccessFail;
 import de.mxro.httpserver.HttpService;
 import de.mxro.httpserver.Request;
@@ -9,12 +10,13 @@ import de.mxro.httpserver.Response;
 
 public class FilterService implements HttpService {
 
+    private final Function<Request, Boolean> test;
     private final HttpService primary;
     private final HttpService secondary;
 
     @Override
     public void stop(final SimpleCallback callback) {
-        primary.start(new SimpleCallback() {
+        primary.stop(new SimpleCallback() {
 
             @Override
             public void onFailure(final Throwable t) {
@@ -31,14 +33,37 @@ public class FilterService implements HttpService {
 
     @Override
     public void start(final SimpleCallback callback) {
-        // TODO Auto-generated method stub
+        secondary.start(new SimpleCallback() {
 
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+
+            @Override
+            public void onSuccess() {
+                primary.start(callback);
+            }
+        });
     }
 
     @Override
     public void process(final Request request, final Response response, final Closure<SuccessFail> callback) {
-        // TODO Auto-generated method stub
 
+        if (test.apply(request)) {
+            this.primary.process(request, response, callback);
+            return;
+        }
+
+        this.secondary.process(request, response, callback);
+
+    }
+
+    public FilterService(final Function<Request, Boolean> test, final HttpService primary, final HttpService secondary) {
+        super();
+        this.test = test;
+        this.primary = primary;
+        this.secondary = secondary;
     }
 
 }
