4fdb26acf1227f289adc0e34b2e41ca6f51b054d
mxro <mxro@nowhere.com>
2014-10-03 17:44:08 +1000

    Fixing issues with GWT JSONP RPC Autocommit Fri Oct 03 17:44:08 EST 2014

--- src/main/java/de/mxro/httpserver/HttpServer.java
+++ src/main/java/de/mxro/httpserver/HttpServer.java
@@ -5,12 +5,16 @@ import de.mxro.httpserver.internal.ResponseData;
 
 public class HttpServer {
 
-	public static Response createResponse() {
-		return new ResponseData();
-	}
-	
-	public static Request createDummyRequest() {
-		return new DummyRequestData();
-	}
-	
+    public static Response createResponse() {
+        return new ResponseData();
+    }
+
+    public static Request createDummyRequest() {
+        return new DummyRequestData();
+    }
+
+    public static Request alterUri(final String newUri, final Request forRequest) {
+
+    }
+
 }
--- src/main/java/de/mxro/httpserver/internal/RequestData.java
+++ src/main/java/de/mxro/httpserver/internal/RequestData.java
@@ -9,11 +9,12 @@ import de.mxro.httpserver.Request;
 
 public final class RequestData implements Request {
 
-    private InputStream inputStream;
-    private byte[] data;
-    private Map<String, String> headers;
-    private String requestUri;
-    private HttpMethod method;
+    private final InputStream inputStream;
+    private final byte[] data;
+    private final Map<String, String> headers;
+    private final String requestUri;
+    private final HttpMethod method;
+    private final Address address;
 
     @Override
     public InputStream getInputStream() {
@@ -48,7 +49,16 @@ public final class RequestData implements Request {
     @Override
     public Address getSourceAddress() {
         return address;
+    }
 
+    public RequestData(final Request request) {
+        super();
+        this.inputStream = request.getInputStream();
+        this.data = request.getData();
+        this.headers = request.getHeaders();
+        this.requestUri = request.getRequestUri();
+        this.method = request.getMethod();
+        this.address = request.getSourceAddress();
     }
 
 }
