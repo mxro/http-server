33804b1ca143f522db7071b02b21e365997647fe
mxro <mxro@nowhere.com>
2014-10-03 17:40:19 +1000

    a

---
+++ src/main/java/de/mxro/httpserver/internal/RequestData.java
@@ -0,0 +1,53 @@
+package de.mxro.httpserver.internal;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import de.mxro.httpserver.Address;
+import de.mxro.httpserver.HttpMethod;
+import de.mxro.httpserver.Request;
+
+public final class RequestData implements Request {
+
+    @Override
+    public InputStream getInputStream() {
+        return null;
+    }
+
+    @Override
+    public byte[] getData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getRequestUri() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getHeader(final String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public HttpMethod getMethod() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Address getSourceAddress() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
