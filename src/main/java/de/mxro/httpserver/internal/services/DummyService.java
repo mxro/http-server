740f9d2c82700d90d5f34e2e00ab1215b6b31fd1
mxro <mxro@nowhere.com>
2014-10-06 08:28:09 +1100

    a

---
+++ src/main/java/de/mxro/httpserver/internal/services/DummyService.java
@@ -0,0 +1,29 @@
+package de.mxro.httpserver.internal.services;
+
+import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.fn.Closure;
+import de.mxro.fn.SuccessFail;
+import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.Request;
+import de.mxro.httpserver.Response;
+
+public class DummyService implements HttpService {
+
+    @Override
+    public void stop(final SimpleCallback callback) {
+        callback.onSuccess();
+    }
+
+    @Override
+    public void start(final SimpleCallback callback) {
+        callback.onSuccess();
+    }
+
+    @Override
+    public void process(final Request request, final Response response, final Closure<SuccessFail> callback) {
+        response.setResponseCode(200);
+        response.setContent("Hello");
+        callback.apply(SuccessFail.success());
+    }
+
+}
