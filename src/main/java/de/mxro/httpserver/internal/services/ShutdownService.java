375e0d88b6fcef433cff42134f84386cd06bec18
mxro <mxro@nowhere.com>
2014-10-15 09:20:23 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 09:20:23 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/ShutdownService.java
+++ src/main/java/de/mxro/httpserver/internal/services/ShutdownService.java
@@ -1,5 +1,37 @@
 package de.mxro.httpserver.internal.services;
 
-public class ShutdownService {
+import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.fn.Closure;
+import de.mxro.fn.SuccessFail;
+import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.Request;
+import de.mxro.httpserver.Response;
+import de.mxro.server.ServerComponent;
+
+public class ShutdownService implements HttpService {
+
+    private final String secret;
+    private final ServerComponent serverToShutdown;
+
+    @Override
+    public void stop(final SimpleCallback callback) {
+        callback.onSuccess();
+    }
+
+    @Override
+    public void start(final SimpleCallback callback) {
+        callback.onSuccess();
+    }
+
+    @Override
+    public void process(final Request request, final Response response, final Closure<SuccessFail> callback) {
+
+    }
+
+    public ShutdownService(final String secret, final ServerComponent serverToShutdown) {
+        super();
+        this.secret = secret;
+        this.serverToShutdown = serverToShutdown;
+    }
 
 }
