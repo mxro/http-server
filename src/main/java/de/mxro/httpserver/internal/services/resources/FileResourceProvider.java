f7557daa598ea63e4d463b9c6593d710c1ce7511
mxro <mxro@nowhere.com>
2014-10-14 16:38:04 +1100

    Reorganizing Netty and http-server projects Autocommit Tue Oct 14 16:38:04 EST 2014

--- src/main/java/de/mxro/httpserver/internal/services/resources/FileResourceProvider.java
+++ src/main/java/de/mxro/httpserver/internal/services/resources/FileResourceProvider.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import nx.restserver.netty.ResourceProvider;
+import de.mxro.httpserver.ResourceProvider;
 
 public final class FileResourceProvider implements ResourceProvider {
 
--- src/main/java/de/mxro/httpserver/internal/services/resources/WebResourceProvider.java
+++ src/main/java/de/mxro/httpserver/internal/services/resources/WebResourceProvider.java
@@ -1,55 +1,55 @@
 package de.mxro.httpserver.internal.services.resources;
 
-import nx.restserver.netty.ResourceProvider;
+import de.mxro.httpserver.ResourceProvider;
 
 public class WebResourceProvider implements ResourceProvider {
 
-	private final ResourceProvider decorated;
+    private final ResourceProvider decorated;
 
-	@Override
-	public Resource getResource(final String path) {
-		final Resource resource = decorated.getResource(path);
+    @Override
+    public Resource getResource(final String path) {
+        final Resource resource = decorated.getResource(path);
 
-		if (resource == null) {
-			return null;
-		}
+        if (resource == null) {
+            return null;
+        }
 
-		String mimetype = resource.getMimetype();
-		if (path.endsWith(".html")) {
-			mimetype = "text/html";
-		} else if (path.endsWith(".js")) {
-			mimetype = "text/javascript"; // correct would be
-											// application/javascript but
-											// text/javascript more compatible
-		} else if (path.endsWith(".png")) {
-			mimetype = "image/png";
-		} else if (path.endsWith(".gif")) {
-			mimetype = "image/gif";
-		} else if (path.endsWith(".css")) {
-			mimetype = "text/css";
-		} else if (path.endsWith(".rpc")) {
-			mimetype = "text/plain";
-		}
+        String mimetype = resource.getMimetype();
+        if (path.endsWith(".html")) {
+            mimetype = "text/html";
+        } else if (path.endsWith(".js")) {
+            mimetype = "text/javascript"; // correct would be
+                                          // application/javascript but
+                                          // text/javascript more compatible
+        } else if (path.endsWith(".png")) {
+            mimetype = "image/png";
+        } else if (path.endsWith(".gif")) {
+            mimetype = "image/gif";
+        } else if (path.endsWith(".css")) {
+            mimetype = "text/css";
+        } else if (path.endsWith(".rpc")) {
+            mimetype = "text/plain";
+        }
 
-		final String closedMimetype = mimetype;
-		return new Resource() {
+        final String closedMimetype = mimetype;
+        return new Resource() {
 
-			@Override
-			public byte[] getData() {
-				return resource.getData();
-			}
+            @Override
+            public byte[] getData() {
+                return resource.getData();
+            }
 
-			@Override
-			public String getMimetype() {
-				return closedMimetype;
-			}
+            @Override
+            public String getMimetype() {
+                return closedMimetype;
+            }
 
-		};
-	}
+        };
+    }
 
-	public WebResourceProvider(final ResourceProvider decorated) {
-		super();
-		this.decorated = decorated;
-	}
+    public WebResourceProvider(final ResourceProvider decorated) {
+        super();
+        this.decorated = decorated;
+    }
 
 }
